65d50177369dc4a0b01d04b541f8af58d02267db
bouzuya <m@bouzuya.net>
2018-02-24 11:35:31 +0900

    change b_layers struct

--- keyboards/lets_split/keymaps/bouzuya/b_layer.c
+++ keyboards/lets_split/keymaps/bouzuya/b_layer.c
@@ -1,45 +1,52 @@
 #include "b_layer.h"
+#include "util.h"
 
-// 0b00000000
-//   ^^^^^^^^
-//   |||+---- back layer number
-//   ||+----- oneshot
-//   |+------ pressed
-//   +------- (unused)
-uint8_t b_layers[32] = {};
+typedef struct
+{
+    uint32_t layer;
+    // 0b00000000
+    //   ^^^^^^^^
+    //   |||+---- (unused)
+    //   ||+----- oneshot
+    //   |+------ pressed
+    //   +------- (unused)
+    uint8_t state;
+} b_layer_state_t;
+
+b_layer_state_t b_layers[32] = {};
 
 // b_layer_oneshot
 
 bool b_layer_oneshot_is_on(uint8_t n)
 {
-    return !!(b_layers[n] & 0b00100000);
+    return !!(b_layers[n].state & 0b00100000);
 }
 
 void b_layer_oneshot_off(uint8_t n)
 {
-    b_layers[n] &= ~0b00100000;
+    b_layers[n].state &= ~0b00100000;
 }
 
 void b_layer_oneshot_on(uint8_t n)
 {
-    b_layers[n] |= 0b00100000;
+    b_layers[n].state |= 0b00100000;
 }
 
 // b_layer_pressed
 
 bool b_layer_pressed_is_on(uint8_t n)
 {
-    return !!(b_layers[n] & 0b01000000);
+    return !!(b_layers[n].state & 0b01000000);
 }
 
 void b_layer_pressed_off(uint8_t n)
 {
-    b_layers[n] &= ~0b01000000;
+    b_layers[n].state &= ~0b01000000;
 }
 
 void b_layer_pressed_on(uint8_t n)
 {
-    b_layers[n] |= 0b01000000;
+    b_layers[n].state |= 0b01000000;
 }
 
 // b_layer oneshot and pressed
@@ -58,10 +65,10 @@ void b_layer_clear_layer(uint8_t n)
 
 uint8_t b_layer_get_layer(uint8_t n)
 {
-    return b_layers[n] & 0b00011111;
+    return biton32(b_layers[n].layer);
 }
 
 void b_layer_set_layer(uint8_t n, uint8_t ln)
 {
-    b_layers[n] = (b_layers[n] & ~0b00011111) | (ln & 0b00011111);
+    b_layers[n].layer = 1UL << ln;
 }
