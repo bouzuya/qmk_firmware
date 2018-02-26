3ef22300a55771ccb1770d934a2a854b5ae492cb
bouzuya <m@bouzuya.net>
2018-02-26 14:00:30 +0900

    disable debug

--- keyboards/lets_split/keymaps/bouzuya/config.h
+++ keyboards/lets_split/keymaps/bouzuya/config.h
@@ -34,6 +34,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define NO_DEBUG
+#define NO_PRINT
+// #define NO_ACTION_LAYER
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_MACRO
